e8ac33d77fc13c6399de8f415d7e24c1abb5d751
imamnura <imam.12ra.kkpi@gmail.com>
2025-11-28 15:34:32 +0700

    initial react install

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./styles/App.css";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>Todo List React Dummy</h1>
+        <p>A simple todo list application built with React.</p>
+      </header>
+
+      <main>
+        <section>
+          <h2>Add New Task</h2>
+          <h2>Filter</h2>
+        </section>
+        <section>
+          <h2>Tasks</h2>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default App;
